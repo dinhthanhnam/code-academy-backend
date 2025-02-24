fee3c5d3cfd2fbc61a6418c9f8c471c80b7f7421
Nam <thanhnamak@gmail.com>
2025-02-24 16:10:55 +0700

    First commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|email|unique:users,email',
+            'password' => 'required|string|max:255|min:8',
+        ]);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token
+        ];
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|max:255|email',
+            'password' => 'required|string|max:255|min:8',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                    'message' => 'Thông tin đăng nhập sai!'
+                ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ],200);
+    }
+}
