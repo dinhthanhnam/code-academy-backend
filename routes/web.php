e5c982b51ec4f4399a117be79e7bb8ff176b7182
Nam <thanhnamak@gmail.com>
2025-03-10 20:09:51 +0700

    add auth check

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,16 @@ Route::post('/login', [AuthController::class, "login"]);
 Route::post('/register', [AuthController::class, "register"]);
 Route::post('/logout', [AuthController::class, "logout"])
     ->middleware('auth');
+
+Route::get('/auth/check', function (Request $request) {
+    if (Auth::check()) {
+        return response()->json([
+            'authenticated' => true,
+            'user' => Auth::user()
+        ]);
+    }
+
+    return response()->json([
+        'authenticated' => false
+    ], 401);
+})->middleware(['web', 'auth']);
